894f3b0f6f663ce1c27a9fe4fdb843487fa0d8f0
unknown <visserho6@gmail.com>
2023-06-18 12:49:42 +0200

    udate

--- tijdelijke .py
+++ tijdelijke .py
@@ -1,22 +1,27 @@
+from helper import decoreer
 
-prijzen = {
-    " aardbei " : 3 ,
-    " vanille" : 4 ,
-    " chocolade" : 5, 
-}
+def print_aanbieding ():
+    prijzen = {
+        " aardbei " : 3 ,
+        " vanille" : 4 ,
+        " chocolade" : 5, 
+    }
 
-aanbieding = prijzen [" aardbei "] * 0.8   # maak gebruik van copy en paste ( aardbei) .
+    aanbieding = prijzen [" aardbei "] * 0.8   # maak gebruik van copy en paste ( aardbei) .
 
-reclame_teks = f" Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts $ { aanbieding}"
-# f = formatted string   // variabel = aanbieding {}
-reclame_teks2 = reclame_teks [:63] # hierbij wordt de aanbieding ook rekenkundig 
-reclame_teks3 = reclame_teks2.upper()
-reclame_teks4 = reclame_teks3.split()
-for el in reclame_teks4:    # el is devariable maar je kan ook bv er gebruiken.
-    if len(el) > 4:          # len is de  teks
-        print ( el.upper()) 
-    else:
-        print ( el.lower())
+    reclame_teks = f" Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts $ { aanbieding}"
+    # f = formatted string   // variabel = aanbieding {}
+    reclame_teks2 = reclame_teks [:63] # hierbij wordt de aanbieding ook rekenkundig 
+    reclame_teks3 = reclame_teks2.upper()
+    reclame_teks4 = reclame_teks3.split()
+    for el in reclame_teks4:    # el is devariable maar je kan ook bv er gebruiken.
+        if len(el) > 4:          # len is de  teks
+            print ( el.upper()) 
+        else:
+            print ( el.lower())
+decoreer ( "aanbiedingen")
+
+print_aanbieding () 
 
 
 
