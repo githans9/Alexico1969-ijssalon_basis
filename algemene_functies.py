436e51bc1f0430fd7ba3dc35139575edf27c970b
unknown <visserho6@gmail.com>
2023-07-28 16:52:48 +0200

    functies toegevoegd

--- algemene_functies.py
+++ algemene_functies.py
@@ -6,13 +6,13 @@ print (mijn_functie_1 (4*4))
 print (mijn_functie_1 (10*10))
 print (mijn_functie_1 (12*12))
 
-def mijn_functie (a=12.3, b=10.5):
+def mijn_functie_2 (a=12.3, b=10.5):
     uitvoer_lijst = []
     uitvoer_lijst.append(a+b)
     uitvoer_lijst.append(a-b)    
     uitvoer_lijst.append(a*b)
     uitvoer_lijst.append(a/b)
     return uitvoer_lijst
-print ( mijn_functie ())
+print ( mijn_functie_2 ())
 
 
--- reclame.py
+++ reclame.py
@@ -1,41 +1,50 @@
 
+from algemene_functies import mijn_functie_2
 
+# # FUNCTIEDEFINITIE
 def aanbieding_1 ( smaak  , prijs   , korting  ):
-    prijs_na_korting = prijs * ( 1 - korting )
+    prijs_na_korting = prijs  - ( korting)
     uitvoer = f" Vandaag in de aanbieding: Emmertje ijs ( 1 liter ) in de smaak { smaak } , van { prijs} euro voor { prijs_na_korting} euro." 
     return uitvoer
+## HOOFDPROGRAMMA
+prijs_na_korting = aanbieding_1 ("aardbei", 4, 1 )
+print (prijs_na_korting)
 
-def inkomsten_totaal ( inkomsten,btw ):
-    lijst_1 = [ 220, 430,125, 160, 205, 90, 345]
-    totaal = 0
-    for bedrag in inkomsten :
-        totaal += bedrag
-        btw_bedrag = totaal * btw
-        uitvoer  = f" Het totaal van alle inkomsten van deze week is { totaal} euro, waarover { btw_bedrag} euro btw betaal dient te worden."
-    return totaal
+def inkomsten_totaal (inkomsten ):
+    return  ( inkomsten )
+lijst_1 = [ 220, 430,125, 160, 205, 90, 345]
+sum = sum ( lijst_1) 
+totaal = sum
+bedrag_excl_btw = sum
+btw = 0.09 * bedrag_excl_btw
+bedrag_incl_btw = bedrag_excl_btw + btw 
+print ( f" het totaal van alle inkomsten van deze week is {totaal} euro,waarover {btw} euro btw betaald dient te worden") 
 
 def laag_en_hoog ( mijn_lijst):
-    uitvoer = []
+    mijn_lijst = [ 220, 430,125, 160, 205, 90, 345]
     laagste = min ( mijn_lijst)
     hoogste = max ( mijn_lijst)
-    uitvoer.append ( laagste)
-    uitvoer.append ( hoogste)
-    return uitvoer
+    mijn_lijst.append ( laagste)
+    mijn_lijst.append (hoogste)
+    return mijn_lijst
+mijn_lijst = laag_en_hoog ( ())
+print ( mijn_lijst)
 
-def gemiddelde ( mijn_lijst):
-    aantal = len ( mijn_lijst)
-    totaal = 0
-    for element in mijn_lijst:
-        totaal  += element
-        gemiddelde = totaal / aantal
-        return f" De gemiddelde inkomsten deze week zijn { gemiddelde} euro."
-    
+def gemiddelde ( lijst_1 ):
+     mijn_lijst = lijst_1
+mijn_lijst_1 =  ( max  ( mijn_lijst))
+mijn_lijst_2 =  ( min ( mijn_lijst))
+mijn_lijst = mijn_lijst_1 + mijn_lijst_2 
+bedrag = mijn_lijst /2
+print (f"De gemiddelde inkomsten deze week zijn { bedrag}  euro ")
+
 def meervoudig ( invoer_lijst):
+    invoer_lijst = [ 10,5,3,2,1,2,9]
     tijdelijk = laag_en_hoog ( invoer_lijst)
-    uitvoer = [ tijdelijk[0],tijdelijk[1]] 
-    return uitvoer 
+    uitvoer = [ tijdelijk [0] , tijdelijk [1]]
+    return uitvoer
 
 def combinatie ( invoer_lijst_2):
     korte_lijst = laag_en_hoog ( invoer_lijst_2)
-    uitvoer = mijn_functie_2 ( korte_lijst[0],korte_lijst[1])
+    uitvoer =  ( korte_lijst[0],korte_lijst[1])
     return uitvoer
